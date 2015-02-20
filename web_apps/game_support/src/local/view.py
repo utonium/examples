f6b38864e5f0f6f9cb763bb8c3b1d10e43fc0271
Kevin Cureton <kevin@bang-splat.com>
2015-02-19 21:59:31 -0800

    Implement the view for user search. Add placeholder pages for displaying a user profile page.

--- web_apps/game_support/src/local/view.py
+++ web_apps/game_support/src/local/view.py
@@ -17,6 +17,7 @@ import string
 import pyramid.renderers
 import pyramid.view
 
+import local.stash
 
 # ---------------------------------------------------------------------------------------------
 # Globals
@@ -46,14 +47,46 @@ def createNewUserPage(request):
     return response
 
 
+@pyramid.view.view_config(route_name='view.create_user', request_method='GET')
+def displayUserPage(request):
+    """ Display a detail page about a user.
+    """
+    results = dict()
+
+    response = pyramid.renderers.render_to_response("local:site/templates/user_detail.jinja2",
+                                                    results,
+                                                    request=request)
+    return response
+
+
 @pyramid.view.view_config(route_name='view.search_users', request_method='GET')
 def searchForUsers(request):
     """ The page for searching for users.
     """
-    # TODO: Get the url parameters for nickname
+    print("DEBUG: Searching for users...")
+
+    results = dict()
+    results['error'] = ""
+    results['users'] = list()
+
+    if 'nickname' not in request.GET or request.GET['nickname'] == "":
+        results['error'] = "Please specify a nick name for the search."
+
+    else:
+        users_stash = local.stash.UsersStash()
+        found_users = users_stash.searchUsers(request.GET['nickname'])
+        
+        tmp_users = list()
+        for nick_name in found_users.keys():
+            user_uid = found_users[nick_name]
+            tmp = dict()
+            tmp['url'] = "/users/%s" % user_uid
+            tmp['nickname'] = nick_name 
+            tmp_users.append(tmp)
+        results['users'] = tmp_users
 
     response = pyramid.renderers.render_to_response("local:site/templates/search_users.jinja2",
-                                                    dict(),
+                                                    results,
                                                     request=request)
     return response
 
